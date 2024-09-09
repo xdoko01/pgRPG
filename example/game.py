940d94e1cd38fd3aee79c7d0a701980a69940458
xdoko01 <otakar.dokoupil@gmail.com>
2024-09-09 16:43:44 +0200

    change dir structure

--- example/game.py
+++ example/game.py
@@ -1,8 +1,19 @@
-import pyrpg
+# Path to the pyrpg engine module
+pyrpg_path = '../pyrpg'
 
-#from pyrpg.main import init
+# Bring pyrpg package onto the path
+import sys, os
+from pathlib import Path
+
+print(f'Path before change: {sys.path}')
+#sys.path.append(os.path.abspath(os.path.join('..', 'pyrpg')))
+sys.path.append(os.path.abspath(Path(pyrpg_path)))
+print(f'Path after change: {sys.path}')
+
+# Now do your import
+from pyrpg.main import init
 
 console = True
 filepath = 'new/tests/12_ai/guard_fight_back_if_ambushed_and_attack_on_sight_or_hear_using_template.jsonc'
 
-pyrpg.main.init(console=console, filepath=filepath, timed=False)
+init(console=console, filepath=filepath, timed=False)
